6edcd967a20fa0e0917506987fc9338e0983b94d
Anna <98224289+AnyaNass@users.noreply.github.com>
2022-07-25 16:27:49 +0300

    another menu.js is added

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,16 +1,39 @@
+// (() => {
+// 	const refs = {
+// 	  openMenuBtn: document.querySelector("[data-menu-open]"),
+// 	  closeMenuBtn: document.querySelector("[data-menu-close]"),
+// 		modal: document.querySelector("[data-menu]"),
+// 		body: document.querySelector("#page"),
+// 	};
+ 
+// 	refs.openMenuBtn.addEventListener("click", toggleModal);
+// 	refs.closeMenuBtn.addEventListener("click", toggleModal);
+ 
+// 	function toggleModal() {
+// 		refs.modal.classList.toggle("is-open");
+// 		refs.body.classList.toggle("no-scroll");
+// 	}
+// })();
+ 
 (() => {
-	const refs = {
-	  openMenuBtn: document.querySelector("[data-menu-open]"),
-	  closeMenuBtn: document.querySelector("[data-menu-close]"),
-		modal: document.querySelector("[data-menu]"),
-		body: document.querySelector("#page"),
-	};
+	const menuBtnOpen = document.querySelector('[data-menu-open]');
+	const menuBtnClose = document.querySelector('[data-menu-close]');
+	const mobileMenuRef = document.querySelector('[data-menu]');
  
-	refs.openMenuBtn.addEventListener("click", toggleModal);
-	refs.closeMenuBtn.addEventListener("click", toggleModal);
+	// змінна body для блокування скролу
+	const body = document.querySelector('body');
  
-	function toggleModal() {
-		refs.modal.classList.toggle("is-open");
-		refs.body.classList.toggle("no-scroll");
+	menuBtnOpen.addEventListener('click', openMenu);
+	menuBtnClose.addEventListener('click', openMenu);
+ 
+	function openMenu() {
+	  const expanded = menuBtnOpen.getAttribute('aria-expanded') === 'true' || false;
+	  menuBtnOpen.setAttribute('aria-expanded', !expanded);
+ 
+	  // відкриття-закриття мобільного меню
+	  mobileMenuRef.classList.toggle('is-open');
+ 
+	  // блокування скролу на body
+	  body.classList.toggle('no-scroll');
 	}
  })();
