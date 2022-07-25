9949725e262bd575e87d8e0dafb4b83bf46a38b9
Anna <98224289+AnyaNass@users.noreply.github.com>
2022-07-25 16:30:47 +0300

    mobile-nav tranform is increased

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,39 +1,17 @@
-// (() => {
-// 	const refs = {
-// 	  openMenuBtn: document.querySelector("[data-menu-open]"),
-// 	  closeMenuBtn: document.querySelector("[data-menu-close]"),
-// 		modal: document.querySelector("[data-menu]"),
-// 		body: document.querySelector("#page"),
-// 	};
- 
-// 	refs.openMenuBtn.addEventListener("click", toggleModal);
-// 	refs.closeMenuBtn.addEventListener("click", toggleModal);
- 
-// 	function toggleModal() {
-// 		refs.modal.classList.toggle("is-open");
-// 		refs.body.classList.toggle("no-scroll");
-// 	}
-// })();
- 
 (() => {
-	const menuBtnOpen = document.querySelector('[data-menu-open]');
-	const menuBtnClose = document.querySelector('[data-menu-close]');
-	const mobileMenuRef = document.querySelector('[data-menu]');
+	const refs = {
+	  openMenuBtn: document.querySelector("[data-menu-open]"),
+	  closeMenuBtn: document.querySelector("[data-menu-close]"),
+		modal: document.querySelector("[data-menu]"),
+		body: document.querySelector("#page"),
+	};
  
-	// змінна body для блокування скролу
-	const body = document.querySelector('body');
+	refs.openMenuBtn.addEventListener("click", toggleModal);
+	refs.closeMenuBtn.addEventListener("click", toggleModal);
  
-	menuBtnOpen.addEventListener('click', openMenu);
-	menuBtnClose.addEventListener('click', openMenu);
- 
-	function openMenu() {
-	  const expanded = menuBtnOpen.getAttribute('aria-expanded') === 'true' || false;
-	  menuBtnOpen.setAttribute('aria-expanded', !expanded);
- 
-	  // відкриття-закриття мобільного меню
-	  mobileMenuRef.classList.toggle('is-open');
- 
-	  // блокування скролу на body
-	  body.classList.toggle('no-scroll');
+	function toggleModal() {
+		refs.modal.classList.toggle("is-open");
+		refs.body.classList.toggle("no-scroll");
 	}
- })();
+})();
+ 
